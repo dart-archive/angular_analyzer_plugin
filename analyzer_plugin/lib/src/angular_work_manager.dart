620d7ebb35c6c29de0af75e0470ff6af75f659c7
Konstantin Shcheglov <scheglov@google.com>
2015-10-09 12:52:47 -0700

    Try to find and prioritize analysis for Dart files corresponding to priority HTML templates.
    R=brianwilkerson@google.com
    BUG=
    
    Review URL: https://codereview.chromium.org//1400503004 .

--- analyzer_plugin/lib/src/angular_work_manager.dart
+++ analyzer_plugin/lib/src/angular_work_manager.dart
@@ -11,9 +11,11 @@ import 'package:analyzer/src/generated/engine.dart'
         InternalAnalysisContext;
 import 'package:analyzer/src/generated/error.dart';
 import 'package:analyzer/src/generated/source.dart';
+import 'package:analyzer/task/dart.dart';
 import 'package:analyzer/task/model.dart';
 import 'package:angular2_analyzer_plugin/src/model.dart';
 import 'package:angular2_analyzer_plugin/src/tasks.dart';
+import 'package:path/path.dart' as pathos;
 
 /**
  * The manager for Angular specific analysis.
@@ -23,6 +25,23 @@ class AngularWorkManager implements WorkManager {
    * The context for which work is being managed.
    */
   final InternalAnalysisContext context;
+
+  /**
+   * The list of Dart sources for which we want to check whether they are
+   * libraries and resolve them to be able to resolve [priorityHtmlSources].
+   */
+  final List<Source> priorityDartSourcesForKind = <Source>[];
+
+  /**
+   * The list of Dart sources for which we want to compute
+   * [VIEWS_WITH_HTML_TEMPLATES] to be able to resolve [priorityHtmlSources].
+   */
+  final List<Source> priorityDartSourcesForViews = <Source>[];
+
+  /**
+   * The list of priority HTML sources to resolve as templates.
+   */
+  final List<Source> priorityHtmlSources = <Source>[];
 
   /**
    * Initialize a newly created manager.
@@ -55,7 +74,24 @@ class AngularWorkManager implements WorkManager {
   }
 
   @override
-  void applyPriorityTargets(List<AnalysisTarget> targets) {}
+  void applyPriorityTargets(List<AnalysisTarget> targets) {
+    priorityDartSourcesForKind.clear();
+    priorityDartSourcesForViews.clear();
+    priorityHtmlSources.clear();
+    for (AnalysisTarget target in targets) {
+      if (_isHtmlSource(target)) {
+        Source htmlSource = target;
+        priorityHtmlSources.add(htmlSource);
+        // Try to find the corresponding Dart source and schedule its analysis.
+        Source dartSource = context.sourceFactory.resolveUri(htmlSource,
+            pathos.withoutExtension(htmlSource.shortName) + '.dart');
+        if (dartSource != null && dartSource.exists()) {
+          priorityDartSourcesForKind.add(dartSource);
+          priorityHtmlSources.add(htmlSource);
+        }
+      }
+    }
+  }
 
   @override
   List<AnalysisError> getErrors(Source source) {
@@ -64,11 +100,45 @@ class AngularWorkManager implements WorkManager {
 
   @override
   TargetedResult getNextResult() {
+    // Request SOURCE_KIND computing.
+    while (priorityDartSourcesForKind.isNotEmpty) {
+      Source source = priorityDartSourcesForKind.last;
+      if (!_needsComputing(source, SOURCE_KIND)) {
+        priorityDartSourcesForKind.removeLast();
+        continue;
+      }
+      return new TargetedResult(source, SOURCE_KIND);
+    }
+    // Request VIEWS_WITH_HTML_TEMPLATES computing.
+    while (priorityDartSourcesForViews.isNotEmpty) {
+      Source source = priorityDartSourcesForViews.last;
+      LibrarySpecificUnit target = new LibrarySpecificUnit(source, source);
+      if (!_needsComputing(target, VIEWS_WITH_HTML_TEMPLATES)) {
+        priorityDartSourcesForViews.removeLast();
+        continue;
+      }
+      return new TargetedResult(target, VIEWS_WITH_HTML_TEMPLATES);
+    }
+    // Request HTML_TEMPLATES computing.
+    while (priorityHtmlSources.isNotEmpty) {
+      Source source = priorityHtmlSources.last;
+      if (!_needsComputing(source, HTML_TEMPLATES)) {
+        priorityHtmlSources.removeLast();
+        continue;
+      }
+      return new TargetedResult(source, HTML_TEMPLATES);
+    }
+    // No results to compute.
     return null;
   }
 
   @override
   WorkOrderPriority getNextResultPriority() {
+    if (priorityDartSourcesForKind.isNotEmpty ||
+        priorityDartSourcesForViews.isNotEmpty ||
+        priorityHtmlSources.isNotEmpty) {
+      return WorkOrderPriority.PRIORITY;
+    }
     return WorkOrderPriority.NONE;
   }
 
@@ -80,12 +150,33 @@ class AngularWorkManager implements WorkManager {
 
   @override
   void resultsComputed(AnalysisTarget target, Map outputs) {
-    List<View> newViews = outputs[VIEWS_WITH_HTML_TEMPLATES];
-    if (newViews != null) {
-      for (View newView in newViews) {
-        _updateTemplateViews(newView, true);
+    // Schedule priority views computing.
+    {
+      SourceKind sourceKind = outputs[SOURCE_KIND];
+      if (sourceKind != null && priorityDartSourcesForKind.remove(target)) {
+        if (sourceKind == SourceKind.LIBRARY) {
+          priorityDartSourcesForViews.add(target);
+        }
       }
     }
+    // Update views containing templates.
+    {
+      List<View> newViews = outputs[VIEWS_WITH_HTML_TEMPLATES];
+      if (newViews != null) {
+        for (View newView in newViews) {
+          _updateTemplateViews(newView, true);
+        }
+      }
+    }
+  }
+
+  /**
+   * Returns `true` if the given [result] of the given [target] needs
+   * computing, i.e. it is not in the valid and not in the error state.
+   */
+  bool _needsComputing(AnalysisTarget target, ResultDescriptor result) {
+    CacheState state = analysisCache.getState(target, result);
+    return state != CacheState.VALID && state != CacheState.ERROR;
   }
 
   void _updateTemplateViews(View view, bool addView) {
@@ -105,7 +196,7 @@ class AngularWorkManager implements WorkManager {
   }
 
   /**
-   * Return `true` if the given target is an HTML source.
+   * Return `true` if the given [target] is an HTML source.
    */
   static bool _isHtmlSource(AnalysisTarget target) {
     return target is Source && AnalysisEngine.isHtmlFileName(target.fullName);
--- analyzer_plugin/test/angular_work_manager_test.dart
+++ analyzer_plugin/test/angular_work_manager_test.dart
@@ -9,6 +9,8 @@ import 'package:analyzer/src/generated/engine.dart'
         ChangeNoticeImpl,
         InternalAnalysisContext;
 import 'package:analyzer/src/generated/source.dart';
+import 'package:analyzer/src/task/dart.dart';
+import 'package:analyzer/task/dart.dart';
 import 'package:analyzer/task/model.dart';
 import 'package:angular2_analyzer_plugin/src/angular_work_manager.dart';
 import 'package:angular2_analyzer_plugin/src/model.dart';
@@ -24,26 +26,42 @@ main() {
 
 @reflectiveTest
 class AngularWorkManagerTest {
+  SourceFactory sourceFactory = new _SourceFactoryMock();
   InternalAnalysisContext context = new _InternalAnalysisContextMock();
   AnalysisCache cache;
   AngularWorkManager manager;
 
-  Source source1 = new _MockSource('1.html');
-  Source source2 = new _MockSource('2.html');
-  Source source3 = new _MockSource('3.html');
-  Source source4 = new _MockSource('4.html');
+  Source source1 = new _SourceMock('1.html');
+  Source source2 = new _SourceMock('2.html');
+  Source source3 = new _SourceMock('3.html');
+  Source source4 = new _SourceMock('4.html');
+  Source dartSource1 = new _SourceMock('1.dart');
+  Source dartSource2 = new _SourceMock('2.dart');
   CacheEntry entry1;
   CacheEntry entry2;
   CacheEntry entry3;
   CacheEntry entry4;
+  CacheEntry dartEntry1;
+  CacheEntry dartEntry2;
+  CacheEntry dartUnitEntry1;
+  CacheEntry dartUnitEntry2;
 
   void setUp() {
+    when(context.sourceFactory).thenReturn(sourceFactory);
     cache = context.analysisCache;
     manager = new AngularWorkManager(context);
+    when(dartSource1.exists()).thenReturn(true);
+    when(dartSource2.exists()).thenReturn(true);
     entry1 = context.getCacheEntry(source1);
     entry2 = context.getCacheEntry(source2);
     entry3 = context.getCacheEntry(source3);
     entry4 = context.getCacheEntry(source4);
+    dartEntry1 = context.getCacheEntry(dartSource1);
+    dartEntry2 = context.getCacheEntry(dartSource2);
+    dartUnitEntry1 = context
+        .getCacheEntry(new LibrarySpecificUnit(dartSource1, dartSource1));
+    dartUnitEntry2 = context
+        .getCacheEntry(new LibrarySpecificUnit(dartSource2, dartSource2));
   }
 
   void test_applyChange_addHtml_emptyTemplateViews() {
@@ -52,14 +70,119 @@ class AngularWorkManagerTest {
     expect(entry1.getValue(TEMPLATE_VIEWS), isEmpty);
   }
 
+  void test_applyPriorityTargets_hasDart() {
+    when(sourceFactory.resolveUri(source1, '1.dart')).thenReturn(dartSource1);
+    manager.applyPriorityTargets(<Source>[source1]);
+    expect(manager.priorityDartSourcesForKind, [dartSource1]);
+  }
+
+  void test_applyPriorityTargets_noDart() {
+    manager.applyPriorityTargets(<Source>[source1]);
+    expect(manager.priorityDartSourcesForKind, isEmpty);
+  }
+
   void test_getNextResult_null() {
     TargetedResult nextResult = manager.getNextResult();
     expect(nextResult, isNull);
   }
 
+  void test_getNextResult_priorityDartSourcesForKind() {
+    manager.priorityDartSourcesForKind.add(dartSource1);
+    TargetedResult nextResult = manager.getNextResult();
+    expect(nextResult, new TargetedResult(dartSource1, SOURCE_KIND));
+    expect(manager.priorityDartSourcesForKind, [dartSource1]);
+  }
+
+  void test_getNextResult_priorityDartSourcesForKind_alreadyComputed() {
+    manager.priorityDartSourcesForKind.add(dartSource1);
+    dartEntry1.setValue(SOURCE_KIND, SourceKind.LIBRARY, []);
+    TargetedResult nextResult = manager.getNextResult();
+    expect(nextResult, null);
+    expect(manager.priorityDartSourcesForKind, isEmpty);
+  }
+
+  void test_getNextResult_priorityDartSourcesForViews() {
+    manager.priorityDartSourcesForViews.add(dartSource1);
+    TargetedResult nextResult = manager.getNextResult();
+    expect(
+        nextResult,
+        new TargetedResult(new LibrarySpecificUnit(dartSource1, dartSource1),
+            VIEWS_WITH_HTML_TEMPLATES));
+    expect(manager.priorityDartSourcesForViews, [dartSource1]);
+  }
+
+  void test_getNextResult_priorityDartSourcesForViews_alreadyComputed() {
+    manager.priorityDartSourcesForViews.add(dartSource1);
+    dartUnitEntry1.setValue(VIEWS_WITH_HTML_TEMPLATES, [], []);
+    TargetedResult nextResult = manager.getNextResult();
+    expect(nextResult, null);
+    expect(manager.priorityDartSourcesForKind, isEmpty);
+  }
+
+  void test_getNextResult_priorityHtmlSources() {
+    manager.priorityHtmlSources.add(source1);
+    TargetedResult nextResult = manager.getNextResult();
+    expect(nextResult, new TargetedResult(source1, HTML_TEMPLATES));
+    expect(manager.priorityHtmlSources, [source1]);
+  }
+
+  void test_getNextResult_priorityHtmlSources_alreadyComputed() {
+    manager.priorityHtmlSources.add(source1);
+    entry1.setValue(HTML_TEMPLATES, [], []);
+    TargetedResult nextResult = manager.getNextResult();
+    expect(nextResult, null);
+    expect(manager.priorityHtmlSources, isEmpty);
+  }
+
   void test_getNextResultPriority_none() {
     WorkOrderPriority priority = manager.getNextResultPriority();
     expect(priority, WorkOrderPriority.NONE);
+  }
+
+  void test_getNextResultPriority_priorityDartSourcesForKind() {
+    manager.priorityDartSourcesForKind.add(dartSource1);
+    WorkOrderPriority priority = manager.getNextResultPriority();
+    expect(priority, WorkOrderPriority.PRIORITY);
+  }
+
+  void test_getNextResultPriority_priorityDartSourcesForViews() {
+    manager.priorityDartSourcesForViews.add(dartSource1);
+    WorkOrderPriority priority = manager.getNextResultPriority();
+    expect(priority, WorkOrderPriority.PRIORITY);
+  }
+
+  void test_getNextResultPriority_priorityHtmlSources() {
+    manager.priorityHtmlSources.add(source1);
+    WorkOrderPriority priority = manager.getNextResultPriority();
+    expect(priority, WorkOrderPriority.PRIORITY);
+  }
+
+  void test_resultsComputed_notSourceKind() {
+    manager.priorityDartSourcesForKind.add(dartSource1);
+    manager.resultsComputed(dartSource1, {SCAN_ERRORS: []});
+    expect(manager.priorityDartSourcesForKind, [dartSource1]);
+    expect(manager.priorityDartSourcesForViews, isEmpty);
+  }
+
+  void test_resultsComputed_sourceKind_forPriorityDartSource_library() {
+    manager.priorityDartSourcesForKind.add(dartSource1);
+    manager.resultsComputed(dartSource1, {SOURCE_KIND: SourceKind.LIBRARY});
+    expect(manager.priorityDartSourcesForKind, isEmpty);
+    expect(manager.priorityDartSourcesForViews, [dartSource1]);
+  }
+
+  void test_resultsComputed_sourceKind_forPriorityDartSource_notLibrary() {
+    manager.priorityDartSourcesForKind.add(dartSource1);
+    manager.resultsComputed(dartSource1, {SOURCE_KIND: SourceKind.PART});
+    expect(manager.priorityDartSourcesForKind, isEmpty);
+    expect(manager.priorityDartSourcesForViews, isEmpty);
+  }
+
+  void test_resultsComputed_sourceKind_notPriorityDartSource() {
+    manager.priorityDartSourcesForKind.add(dartSource1);
+    manager.resultsComputed(dartSource2, {SOURCE_KIND: SourceKind.LIBRARY});
+    expect(manager.priorityDartSourcesForKind, [dartSource1]);
+    expect(manager.priorityDartSourcesForViews, isEmpty);
   }
 
   void test_resultsComputed_viewsWithHtmlTemplates() {
@@ -121,9 +244,13 @@ class _InternalAnalysisContextMock extends TypedMock
   noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
 }
 
-class _MockSource extends TypedMock implements Source {
+class _SourceFactoryMock extends TypedMock implements SourceFactory {
+  noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
+}
+
+class _SourceMock extends TypedMock implements Source {
   final String shortName;
-  _MockSource(this.shortName);
+  _SourceMock(this.shortName);
   @override
   String get fullName => '/' + shortName;
   noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
