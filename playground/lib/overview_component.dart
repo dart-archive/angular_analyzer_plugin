4d998b54ceb7ca4a088ff6855fa630b4d983c551
Mike Fairhurst <mfairhurst@google.com>
2016-11-18 16:08:58 -0800

    Add playground for testing the plugin against

---
+++ playground/lib/overview_component.dart
@@ -0,0 +1,12 @@
+import 'package:angular2/angular2.dart';
+import 'counter_component.dart';
+import 'bubbled_directive.dart';
+
+@Component(selector: 'blah', directives: const[CounterComponent, NgFor, NgIf, BubbledDirective], templateUrl: 'overview_component.html')
+class OverviewComponent {
+  String header;
+  List<String> items;
+
+  OtherComponent() {
+  }
+}
