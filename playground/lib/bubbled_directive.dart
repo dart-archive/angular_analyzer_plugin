915c83f5cc0675870e45fe6012fd8dc9db4f2f98
Mike Fairhurst <mfairhurst@google.com>
2016-12-06 10:48:11 -0800

    Make the playground runnable, also some tweaks to it that I've made recently

--- playground/lib/bubbled_directive.dart
+++ playground/lib/bubbled_directive.dart
@@ -2,8 +2,11 @@ import 'package:angular2/angular2.dart';
 
 @Directive(selector: "[bubbled]", exportAs: 'bubble')
 class BubbledDirective {
+  int _width;
 
-  @Input("BubbleWidth") int width;
-  @Output() EventEmitter popped = new EventEmitter();
+  @Input("bubbleWidth") set width(int x) => _width = x;
+  int get width => _width;
+
+  @Output() EventEmitter<int> get popped => new EventEmitter();
 
 }
--- playground/lib/overview_component.dart
+++ playground/lib/overview_component.dart
@@ -2,10 +2,11 @@ import 'package:angular2/angular2.dart';
 import 'counter_component.dart';
 import 'bubbled_directive.dart';
 
-@Component(selector: 'blah', directives: const[CounterComponent, NgFor, NgIf, BubbledDirective], templateUrl: 'overview_component.html')
+@Component(selector: 'overview-component', directives: const[CounterComponent, NgFor, NgIf, BubbledDirective], templateUrl: 'overview_component.html')
 class OverviewComponent {
   String header;
   List<String> items;
+  int count;
 
   OtherComponent() {
   }
