7b53fbf3a794a34dbc00b8978b78f123e42b3e38
Mike Fairhurst <mfairhurst@google.com>
2016-12-09 18:22:59 -0800

    Change to use pub for non-sdk dependencies, to save myself version hell

--- tools/get_deps.sh
+++ tools/get_deps.sh
@@ -1,14 +1,3 @@
-echo Creating deps directory
-mkdir deps
-cd deps
-echo done
-echo
-
-echo Pulling down tuple
-git clone https://github.com/kseo/tuple.git tuple
-echo done
-echo
-
 echo Using depot tools to pull in the SDK
 gclient config https://github.com/dart-lang/sdk.git
 gclient sync
--- tools/update_deps.sh
+++ tools/update_deps.sh
@@ -1,11 +1,10 @@
-echo Updating tuple
-cd deps/tuple
-git pull
+echo Updating pub deps
+cd deps
+pub get
 echo done
 echo
 
 echo Updating the sdk with depot_tools
-cd ..
 gclient sync
 echo done
 echo
@@ -13,11 +12,21 @@ echo
 cd ..
 
 echo Copying and transforming sdk .packages to be useful to this repo
-cat deps/sdk/.packages | sed 's/:/:deps\/sdk\//' > .packages
+cat deps/sdk/.packages | grep -v '^test' | sed 's/:/:deps\/sdk\//' > .packages
 echo done
 echo
 
-echo Adding tuple to .packages
-echo 'tuple:deps/tuple/lib' >> .packages
+echo Copying pub .packages to be useful to this repo
+cat deps/.packages >> .packages
+echo done
+echo
+
+echo Adding analyzer plugin to .packages
+echo 'angular_analyzer_plugin:analyzer_plugin/lib' >> .packages
+echo done
+echo
+
+echo Adding server to .packages
+echo 'angular_analyzer_server_plugin:server_plugin/lib' >> .packages
 echo done
 echo
