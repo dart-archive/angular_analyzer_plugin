fcd746c9ff0bd945b5f58cd4dd4075715c87bb0b
Michael R Fairhurst <michaelrfairhurst@gmail.com>
2017-05-11 14:45:04 -0700

    Get new plugin architecture in a workable development stage (#316)
    See readme for how to get set up....currently doesn't do anything,
    though.

--- playground/lib/overview_component.dart
+++ playground/lib/overview_component.dart
@@ -2,6 +2,8 @@ import 'package:angular2/angular2.dart';
 import 'counter_component.dart';
 import 'bubbled_directive.dart';
 
+import 'package:analyze_angular/analyze_angular.dart';
+
 @Component(selector: 'blah',
     directives: const[CounterComponent, NgFor, NgIf, BubbledDirective],
     templateUrl: 'overview_component.html'
