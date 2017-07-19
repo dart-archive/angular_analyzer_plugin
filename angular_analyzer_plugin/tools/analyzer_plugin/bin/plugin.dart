e33c58b0a86896711c00b07419916f77e9644cad
Michael R Fairhurst <michaelrfairhurst@gmail.com>
2017-07-19 13:54:48 -0700

    Fix starter loaded package (#393)

--- angular_analyzer_plugin/tools/analyzer_plugin/bin/plugin.dart
+++ angular_analyzer_plugin/tools/analyzer_plugin/bin/plugin.dart
@@ -1,6 +1,6 @@
 import 'dart:isolate';
 
-import 'package:angular_analysis_plugin/starter.dart';
+import 'package:angular_analyzer_plugin/starter.dart';
 
 void main(List<String> args, SendPort sendPort) {
   start(args, sendPort);
