a5ed813abe17323e0236a1c9de826cd57afb0a14
Brian Wilkerson <brianwilkerson@google.com>
2017-08-09 14:07:50 -0700

    address comments

--- playground/lib/overview_component.dart
+++ playground/lib/overview_component.dart
@@ -2,6 +2,8 @@ import 'package:angular2/angular2.dart';
 import 'counter_component.dart';
 import 'bubbled_directive.dart';
 
+import 'package:angular_analyzer_plugin/enable.dart';
+
 @Component(selector: 'blah',
     directives: const[CounterComponent, NgFor, NgIf, BubbledDirective],
     templateUrl: 'overview_component.html'
