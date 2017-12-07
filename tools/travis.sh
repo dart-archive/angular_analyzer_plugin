4b504f8a9eb97c29a961bb37c5b5c41f570afb9f
Mike Fairhurst <mfairhurst@google.com>
2017-12-07 13:25:54 -0800

    Test with pub in travis, except for specific branches.
    I'll likely keep one branch that's in sync with the SDK at head. To
    maintain it, I'll make new PRs to merge in.
    
    For that reason, go off of branch name to decide whether or not to
    leave the script functionally working as is (which uses the latest), or
    whether to run pub get to avoid having to work against the latest sdk
    packages at all times.

--- tools/travis.sh
+++ tools/travis.sh
@@ -3,8 +3,26 @@
 # Fast fail the script on failures.
 set -e
 
-# Go to the respective package directory and resolve pub dependencies.
+# Go to the respective package directory
 cd $PACKAGE
+
+pwd
+# Check if we should resolve pub dependencies, or use what was fetched from
+# depot tools.
+if [ -z "$(git branch | grep 'SDK_AT_HEAD')" ]
+then
+  echo Using pub for the SDK dependencies.
+  echo
+  echo To test against the SDK at master, include SDK_AT_HEAD in your branch
+  echo name. Otherwise, we will test with pub.
+
+  pub get
+else
+  echo Using depot_tools for the SDK dependencies.
+  echo
+  echo Because your branch name includes SDK_AT_HEAD, this will test against
+  echo the latest SDK source instead of using pub.
+fi
 
 # Analyze the test first
 dartanalyzer lib test
