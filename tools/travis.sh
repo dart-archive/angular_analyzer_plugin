1acad4f71e948b3449a4fa2214689263617cfd03
Mike Fairhurst <mfairhurst@google.com>
2017-12-07 15:04:53 -0800

    Split old_plugin_loader checks further; different error message, fix syntax

--- tools/travis.sh
+++ tools/travis.sh
@@ -6,22 +6,29 @@ set -e
 # Go to the respective package directory
 cd $PACKAGE
 
-pwd
 # Check if we should resolve pub dependencies, or use what was fetched from
 # depot tools.
-if [ -z "$(git branch | grep 'SDK_AT_HEAD')"  && "$PACKAGE" != old_plugin_loader ]
+if [[ "$PACKAGE" != old_plugin_loader ]]
 then
-  echo Using pub for the SDK dependencies.
-  echo
-  echo To test against the SDK at master, include SDK_AT_HEAD in your branch
-  echo name. Otherwise, we will test with pub.
-
-  pub get
+  if [[ -z "$(git branch | grep 'SDK_AT_HEAD')" ]]
+  then
+    echo Using pub for the SDK dependencies.
+    echo
+    echo To test against the SDK at master, include SDK_AT_HEAD in your branch
+    echo name. Otherwise, we will test with pub.
+  
+    pub get
+  else
+    echo Using depot_tools for the SDK dependencies.
+    echo
+    echo Because your branch name includes SDK_AT_HEAD, this will test against
+    echo the latest SDK source instead of using pub.
+  fi
 else
   echo Using depot_tools for the SDK dependencies.
   echo
-  echo Because your branch name includes SDK_AT_HEAD, this will test against
-  echo the latest SDK source instead of using pub.
+  echo The old_plugin_loader relies on packages not published by pub, and so
+  echo can only be tested by using depot_tools.
 fi
 
 # Analyze the test first
