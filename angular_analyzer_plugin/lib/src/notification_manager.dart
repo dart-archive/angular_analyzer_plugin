7722044c82e5cd52c4a76c3bae83f5c2e4b6abc9
Michael R Fairhurst <michaelrfairhurst@gmail.com>
2018-03-24 22:09:18 -0700

    remove unnecessary imports (#526)

--- angular_analyzer_plugin/lib/src/notification_manager.dart
+++ angular_analyzer_plugin/lib/src/notification_manager.dart
@@ -1,9 +1,5 @@
 import 'package:analyzer/error/error.dart';
 import 'package:analyzer/src/generated/source.dart';
-import 'package:analyzer_plugin/channel/channel.dart';
-import 'package:analyzer_plugin/protocol/protocol_constants.dart' as plugin;
-import 'package:analyzer_plugin/protocol/protocol_generated.dart' as plugin;
-import 'package:analyzer_plugin/utilities/analyzer_converter.dart';
 import 'package:angular_analyzer_plugin/notification_manager.dart';
 
 // TODO(mfairhurst) remove NotificationManager & old plugin loader.
