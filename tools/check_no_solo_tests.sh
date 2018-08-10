f179c0f7bed8babe60c524edd353bff148406d4b
Mike Fairhurst <mfairhurst@google.com>
2018-08-10 11:23:22 -0700

    Catch @soloTest in addition to solo_test

--- tools/check_no_solo_tests.sh
+++ tools/check_no_solo_tests.sh
@@ -4,7 +4,7 @@
 set -e
 
 # Look for solo_test in test/*, and catch the "fast fail" error when no results
-GREP_OUT=$(grep solo_test $PACKAGE/test -r || :)
+GREP_OUT=$(egrep 'solo(T|_t)est' $PACKAGE/test -r || :)
 if [[ ! -z "$GREP_OUT" ]]; then
   printf "$PACKAGE is skipping tests due to solo_test(s): \n$GREP_OUT\n"
   exit 1
