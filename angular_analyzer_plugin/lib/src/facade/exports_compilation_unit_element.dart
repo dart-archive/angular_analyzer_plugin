bfe92bede8a69588b1e1fae3db1f8910a8a2250e
Paul Berry <paulberry@google.com>
2018-08-14 14:36:18 -0700

    Rework facade classes to make use of new analyzer "wrapped" classes.

--- angular_analyzer_plugin/lib/src/facade/exports_compilation_unit_element.dart
+++ angular_analyzer_plugin/lib/src/facade/exports_compilation_unit_element.dart
@@ -1,19 +1,15 @@
-import 'package:analyzer/dart/ast/ast.dart' hide Directive;
 import 'package:analyzer/dart/element/element.dart';
-import 'package:analyzer/src/generated/engine.dart' show AnalysisContext;
-import 'package:analyzer/src/generated/java_engine.dart';
-import 'package:analyzer/src/generated/source.dart';
+import 'package:analyzer/src/dart/element/wrapped.dart';
 import 'package:angular_analyzer_plugin/src/model.dart';
 
 /// A facade for a [CompilationUnitElement] which consists only of a component's
 /// exports, and the component itself.
-class ExportsLimitedCompilationUnitFacade implements CompilationUnitElement {
+class ExportsLimitedCompilationUnitFacade extends WrappedCompilationUnitElement {
   final Component _component;
-  final CompilationUnitElement _wrappedUnit;
   LibraryElement libraryFacade;
 
-  ExportsLimitedCompilationUnitFacade(this._wrappedUnit, this._component,
-      {this.libraryFacade});
+  ExportsLimitedCompilationUnitFacade(CompilationUnitElement wrappedUnit, this._component,
+                                      {this.libraryFacade}) : super(wrappedUnit);
 
   @override
   List<PropertyAccessorElement> get accessors =>
@@ -21,15 +17,6 @@ class ExportsLimitedCompilationUnitFacade implements CompilationUnitElement {
           .where(_fromThisUnit)
           .map((export) => export.element)
           .where((element) => element is PropertyAccessorElement));
-
-  @override
-  AnalysisContext get context => _wrappedUnit.context;
-
-  @override
-  String get displayName => _wrappedUnit.displayName;
-
-  @override
-  String get documentationComment => _wrappedUnit.documentationComment;
 
   @override
   LibraryElement get enclosingElement => libraryFacade;
@@ -51,107 +38,13 @@ class ExportsLimitedCompilationUnitFacade implements CompilationUnitElement {
   List<FunctionTypeAliasElement> get functionTypeAliases => [];
 
   @override
-  bool get hasAlwaysThrows => _wrappedUnit.hasAlwaysThrows;
-
-  @override
-  bool get hasDeprecated => _wrappedUnit.hasDeprecated;
-
-  @override
-  bool get hasFactory => _wrappedUnit.hasFactory;
-
-  @override
-  bool get hasIsTest => _wrappedUnit.hasIsTest;
-
-  @override
-  bool get hasIsTestGroup => _wrappedUnit.hasIsTestGroup;
-
-  @override
   bool get hasJS => false;
-
-  @override
-  bool get hasLoadLibraryFunction => _wrappedUnit.hasLoadLibraryFunction;
-
-  @override
-  bool get hasOverride => _wrappedUnit.hasOverride;
-
-  @override
-  bool get hasProtected => _wrappedUnit.hasProtected;
-
-  @override
-  bool get hasRequired => _wrappedUnit.hasRequired;
-
-  @override
-  bool get hasVisibleForTemplate => _wrappedUnit.hasVisibleForTemplate;
-
-  @override
-  bool get hasVisibleForTesting => _wrappedUnit.hasVisibleForTesting;
-
-  @override
-  int get id => _wrappedUnit.id;
-
-  @override
-  bool get isAlwaysThrows => hasAlwaysThrows;
-
-  @override
-  bool get isDeprecated => hasDeprecated;
-
-  @override
-  bool get isFactory => hasFactory;
 
   @override
   bool get isJS => false;
 
   @override
-  bool get isOverride => hasOverride;
-
-  @override
-  bool get isPrivate => _wrappedUnit.isPrivate;
-
-  @override
-  bool get isProtected => hasProtected;
-
-  @override
-  bool get isPublic => _wrappedUnit.isPublic;
-
-  @override
-  bool get isRequired => hasRequired;
-
-  @override
-  bool get isSynthetic => _wrappedUnit.isSynthetic;
-
-  @override
-  bool get isVisibleForTesting => hasVisibleForTesting;
-
-  @override
-  ElementKind get kind => _wrappedUnit.kind;
-
-  @override
   LibraryElement get library => libraryFacade;
-
-  @override
-  Source get librarySource => _wrappedUnit.librarySource;
-
-  @override
-  LineInfo get lineInfo => _wrappedUnit.lineInfo;
-
-  @override
-  ElementLocation get location => _wrappedUnit.location;
-
-  @override
-  List<ElementAnnotation> get metadata => _wrappedUnit.metadata;
-
-  @override
-  String get name => _wrappedUnit.name;
-
-  @override
-  int get nameLength =>
-      _wrappedUnit.nameLength; // ignore: deprecated_member_use
-
-  @override
-  int get nameOffset => _wrappedUnit.nameOffset;
-
-  @override
-  Source get source => _wrappedUnit.source;
 
   @override
   List<TopLevelVariableElement> get topLevelVariables => [];
@@ -164,50 +57,12 @@ class ExportsLimitedCompilationUnitFacade implements CompilationUnitElement {
     ..add(_component.classElement);
 
   @override
-  CompilationUnit get unit => _wrappedUnit.unit;
-
-  @override
-  String get uri => _wrappedUnit.uri;
-
-  @override
-  int get uriEnd => _wrappedUnit.uriEnd;
-
-  @override
-  int get uriOffset => _wrappedUnit.uriOffset;
-
-  @override
-  T accept<T>(ElementVisitor<T> visitor) => _wrappedUnit.accept(visitor);
-
-  @override
-  String computeDocumentationComment() =>
-      _wrappedUnit.computeDocumentationComment();
-
-  @override
-  CompilationUnit computeNode() => _wrappedUnit.computeNode();
-
-  @override
-  E getAncestor<E extends Element>(Predicate<Element> predicate) =>
-      _wrappedUnit.getAncestor(predicate); // currently never exported
-
-  @override
   ClassElement getEnum(String name) =>
       enums.firstWhere((_enum) => _enum.name == name, orElse: () => null);
 
   @override
-  String getExtendedDisplayName(String shortName) => _wrappedUnit
-      .getExtendedDisplayName(shortName); // currently never exported
-
-  @override
   ClassElement getType(String className) =>
       types.firstWhere((type) => type.name == name, orElse: () => null);
-
-  @override
-  bool isAccessibleIn(LibraryElement library) =>
-      _wrappedUnit.isAccessibleIn(library);
-
-  @override
-  void visitChildren(ElementVisitor visitor) =>
-      _wrappedUnit.visitChildren(visitor);
 
   // CompilationUnitFacade's are not used for imports, which have prefixes
   bool _fromThisUnit(ExportedIdentifier export) => export.prefix == '';
--- angular_analyzer_plugin/lib/src/facade/exports_import_element.dart
+++ angular_analyzer_plugin/lib/src/facade/exports_import_element.dart
@@ -1,187 +1,32 @@
-import 'package:analyzer/dart/ast/ast.dart' hide Directive;
 import 'package:analyzer/dart/element/element.dart';
-import 'package:analyzer/src/dart/resolver/scope.dart';
-import 'package:analyzer/src/generated/engine.dart' show AnalysisContext;
-import 'package:analyzer/src/generated/java_engine.dart';
-import 'package:analyzer/src/generated/source.dart';
+import 'package:analyzer/src/dart/element/wrapped.dart';
 import 'package:angular_analyzer_plugin/src/facade/exports_library_element.dart';
 import 'package:angular_analyzer_plugin/src/model.dart';
 
 /// A facade for a [ImportElement] which consists only of a component's
 /// exports
-class ExportsImportElementFacade implements ImportElement {
+class ExportsImportElementFacade extends WrappedImportElement {
   final Component _component;
-  final ImportElement _wrappedImport;
   LibraryElement libraryFacade;
 
-  ExportsImportElementFacade(this._wrappedImport, this._component,
-      {this.libraryFacade});
-
-  @override
-  List<NamespaceCombinator> get combinators => _wrappedImport.combinators;
-
-  @override
-  AnalysisContext get context => _wrappedImport.context;
-
-  @override
-  String get displayName => _wrappedImport.displayName;
-
-  @override
-  String get documentationComment => _wrappedImport.documentationComment;
+  ExportsImportElementFacade(ImportElement wrappedImport, this._component,
+                             {this.libraryFacade}) : super(wrappedImport);
 
   @override
   LibraryElement get enclosingElement => libraryFacade;
 
   @override
-  bool get hasAlwaysThrows => _wrappedImport.hasAlwaysThrows;
-
-  @override
-  bool get hasDeprecated => _wrappedImport.hasDeprecated;
-
-  @override
-  bool get hasFactory => _wrappedImport.hasFactory;
-
-  @override
-  bool get hasIsTest => _wrappedImport.hasIsTest;
-
-  @override
-  bool get hasIsTestGroup => _wrappedImport.hasIsTestGroup;
-
-  @override
   bool get hasJS => false;
 
   @override
-  bool get hasOverride => _wrappedImport.hasOverride;
-
-  @override
-  bool get hasProtected => _wrappedImport.hasProtected;
-
-  @override
-  bool get hasRequired => _wrappedImport.hasRequired;
-
-  @override
-  bool get hasVisibleForTemplate => _wrappedImport.hasVisibleForTemplate;
-
-  @override
-  bool get hasVisibleForTesting => _wrappedImport.hasVisibleForTesting;
-
-  @override
-  int get id => _wrappedImport.id;
-
-  @override
-  LibraryElement get importedLibrary => _wrappedImport.importedLibrary == null
+  LibraryElement get importedLibrary => wrappedImport.importedLibrary == null
       ? null
-      : new ExportsLibraryFacade(_wrappedImport.importedLibrary, _component,
+      : new ExportsLibraryFacade(wrappedImport.importedLibrary, _component,
           prefix: prefix?.name);
-
-  @override
-  bool get isAlwaysThrows => hasAlwaysThrows;
-
-  @override
-  bool get isDeferred => _wrappedImport.isDeferred;
-
-  @override
-  bool get isDeprecated => hasDeprecated;
-
-  @override
-  bool get isFactory => hasFactory;
 
   @override
   bool get isJS => false;
 
   @override
-  bool get isOverride => hasOverride;
-
-  @override
-  bool get isPrivate => _wrappedImport.isPrivate;
-
-  @override
-  bool get isProtected => hasProtected;
-
-  @override
-  bool get isPublic => _wrappedImport.isPublic;
-
-  @override
-  bool get isRequired => hasRequired;
-
-  @override
-  bool get isSynthetic => _wrappedImport.isSynthetic;
-
-  @override
-  bool get isVisibleForTesting => hasVisibleForTesting;
-
-  @override
-  ElementKind get kind => _wrappedImport.kind;
-
-  @override
   LibraryElement get library => libraryFacade;
-
-  @override
-  Source get librarySource => _wrappedImport.librarySource;
-
-  @override
-  ElementLocation get location => _wrappedImport.location;
-
-  @override
-  List<ElementAnnotation> get metadata => _wrappedImport.metadata;
-
-  @override
-  String get name => _wrappedImport.name;
-
-  @override
-  int get nameLength => _wrappedImport.nameLength;
-
-  @override
-  int get nameOffset => _wrappedImport.nameOffset;
-
-  @override
-  Namespace get namespace => _wrappedImport.namespace;
-
-  @override
-  PrefixElement get prefix =>
-      _wrappedImport.prefix; // ignore: deprecated_member_use
-
-  @override
-  int get prefixOffset => _wrappedImport.prefixOffset;
-
-  @override
-  Source get source => _wrappedImport.source;
-
-  @override
-  CompilationUnit get unit => _wrappedImport.unit;
-
-  @override
-  String get uri => _wrappedImport.uri;
-
-  @override
-  int get uriEnd => _wrappedImport.uriEnd;
-
-  @override
-  int get uriOffset => _wrappedImport.uriOffset;
-
-  @override
-  T accept<T>(ElementVisitor<T> visitor) => _wrappedImport.accept(visitor);
-
-  @override
-  String computeDocumentationComment() =>
-      _wrappedImport.computeDocumentationComment();
-
-  @override
-  AstNode computeNode() => _wrappedImport.computeNode();
-
-  @override
-  E getAncestor<E extends Element>(Predicate<Element> predicate) =>
-      _wrappedImport.getAncestor(predicate);
-
-  @override
-  String getExtendedDisplayName(String shortName) =>
-      _wrappedImport.getExtendedDisplayName(shortName);
-
-  @override
-  bool isAccessibleIn(LibraryElement library) =>
-      _wrappedImport.isAccessibleIn(library);
-
-  @override
-  void visitChildren(ElementVisitor visitor) =>
-      _wrappedImport.visitChildren(visitor);
 }
