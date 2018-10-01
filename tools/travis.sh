3bb82029b8776cdc0e27a61050cbbb2707dbb795
Michael R Fairhurst <michaelrfairhurst@gmail.com>
2018-10-01 13:35:30 -0700

    Remove old plugin loader (#647)

--- tools/travis.sh
+++ tools/travis.sh
@@ -13,27 +13,19 @@ cd $PACKAGE
 
 # Check if we should resolve pub dependencies, or use what was fetched from
 # depot tools.
-if [[ "$PACKAGE" != old_plugin_loader ]]
+if [[ -z "$(echo $TRAVIS_BRANCH | grep 'SDK_AT_HEAD')" ]]
 then
-  if [[ -z "$(echo $TRAVIS_BRANCH | grep 'SDK_AT_HEAD')" ]]
-  then
-    echo Using pub for the SDK dependencies.
-    echo
-    echo To test against the SDK at master, include SDK_AT_HEAD in your branch
-    echo name. Otherwise, we will test with pub.
-  
-    pub get
-  else
-    echo Using depot_tools for the SDK dependencies.
-    echo
-    echo Because your branch name includes SDK_AT_HEAD, this will test against
-    echo the latest SDK source instead of using pub.
-  fi
+  echo Using pub for the SDK dependencies.
+  echo
+  echo To test against the SDK at master, include SDK_AT_HEAD in your branch
+  echo name. Otherwise, we will test with pub.
+
+  pub get
 else
   echo Using depot_tools for the SDK dependencies.
   echo
-  echo The old_plugin_loader relies on packages not published by pub, and so
-  echo can only be tested by using depot_tools.
+  echo Because your branch name includes SDK_AT_HEAD, this will test against
+  echo the latest SDK source instead of using pub.
 fi
 
 # Analyze the test first
