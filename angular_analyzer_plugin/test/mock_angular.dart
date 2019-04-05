fc67eed0903fafbf33441e4ed7a6e07d71605b9d
Mike Fairhurst <mfairhurst@google.com>
2019-04-04 17:24:11 -0700

    Add copyright header

--- angular_analyzer_plugin/test/mock_angular.dart
+++ angular_analyzer_plugin/test/mock_angular.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2019, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 void addAngularSources(Function(String, [String]) newSource,
     {bool includeQueryList: false}) {
   newSource('/angular2/angular2.dart', r'''
