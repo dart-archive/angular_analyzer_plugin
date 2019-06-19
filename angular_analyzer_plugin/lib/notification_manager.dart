47d562cd587638bb63979c27b34ece5bc493346b
Kevin Moore <kevmoo@google.com>
2019-06-19 13:20:49 -0700

    point folks to the angular repo

--- angular_analyzer_plugin/lib/notification_manager.dart
+++
@@ -1,7 +0,0 @@
-import 'package:analyzer/error/error.dart';
-import 'package:analyzer/src/generated/source.dart';
-
-abstract class NotificationManager {
-  void recordAnalysisErrors(
-      String path, LineInfo lineInfo, List<AnalysisError> analysisErrors);
-}
